61e7a7f706052a1860e0292d2dfe6e5ae5616fe3
Marco Siqueira Campos <marco@siqueiracampos.com>
2016-10-01 23:24:16 -0300

    Add files via upload

---
+++ MASC-SQL-TD.sql
@@ -0,0 +1,10 @@
+
+-- -----------------------------------------
+-- CUNY MSDA DATA 607 -- 
+-- Assignment - Tidying and transformation data
+-- Marco Siqueira Campos
+-- 09/30/2016
+-- -----------------------------------------
+
+
+
